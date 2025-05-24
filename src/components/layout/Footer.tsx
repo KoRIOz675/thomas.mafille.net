c2c11a9807d9c1e8940b13cae07c3f2374d1496f
KoRIOz675 <thomas@mafille.net>
2025-05-24 14:33:10 +0200

    MEDIUM: ci: Updatd header + added prettier for formating

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-gray-800 py-4 text-white">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} My Website. All rights reserved.
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,11 @@
 function Header() {
   return (
-    <header className="bg-gray-200 text-black p-4 dark:bg-gray-800 dark:text-gray-200">
+    <header className="flex flex-col items-center bg-gray-300 p-4 text-black md:flex-row md:justify-around">
       <a href="/" className="text-2xl font-bold">
         <img
           src="/logo/logo.svg"
           alt="Logo"
-          className="inline-block h-16 mr-2"
+          className="mr-2 inline-block h-16"
         />
       </a>
       <nav className="mt-2">
@@ -27,6 +27,20 @@ function Header() {
           </li>
         </ul>
       </nav>
+      <div className="mt-2">
+        <a
+          href="https://github.com/KoRIOz675/thomas.mafille.net"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/github/github-mark.svg"
+            alt="GitHub Logo"
+            className="h-8 w-8"
+          />
+          <span className="sr-only">GitHub Repository</span>
+        </a>
+      </div>
     </header>
   );
 }
